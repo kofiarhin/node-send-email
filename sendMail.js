9233254213108e548df5ee87fbced049f5cac258
mac <mac@esco-3.local>
2022-06-14 10:24:44 +0000

    update to sendmail

--- sendMail.js
+++ sendMail.js
@@ -1,28 +1,30 @@
-var nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer");
 
 function sendMail(address, subject, body) {
-  var transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.email,
-      pass: process.env.password,
-    },
-  });
+  return new Promise((resolve, reject) => {
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
 
-  var mailOptions = {
-    from: process.env.email,
-    to: address,
-    subject: subject,
-    text: body,
-    // html: '<h1>Hi Smartherd</h1><p>Your Messsage</p>'
-  };
+    var mailOptions = {
+      from: process.env.EMAIL,
+      to: address,
+      subject: subject,
+      text: body,
+      // html: '<h1>Hi Smartherd</h1><p>Your Messsage</p>'
+    };
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error.message);
-    } else {
-      console.log("Email sent: " + info.response);
-    }
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        reject({ error: "there was a problemn sending mail" });
+      } else {
+        resolve({ message: "message sent" });
+      }
+    });
   });
 }
 
